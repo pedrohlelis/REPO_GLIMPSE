4f8eb450d098014d4edff23dcfc940f353063334
DevMatheus2 <patumcam@gmail.com>
2024-03-27 19:33:10 -0300

    Test 2 branchMatheus

--- models/User.cs
+++ models/User.cs
@@ -12,7 +12,7 @@ namespace WorkspaceGlimpse
         private String _ProfilePic;
 
         public int IdUsuario {get{return _UserId;} set{_UserId = value;}}
-        public String UserName {get{return _UserName;} set{}}
+        public String UserName {get{return _UserName;} set{_UserName = value;}}
         public String UserEmail {get{return _UserEmail;} set{}}
         public String UserPassword {get{return _UserPassword;} set{}}
         public String ProfilePic {get{return _ProfilePic;} set{}}
