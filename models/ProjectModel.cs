aeddeca829392871c4d70b7196b90b72b3af5635
victorhtanaka <victorhtanaka555@gmail.com>
2024-03-27 20:02:00 -0300

    Fixed dbcontext

--- models/ProjectModel.cs
+++ models/ProjectModel.cs
@@ -4,6 +4,9 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace WorkspaceGlimpse.Models {
     public class Project {
         [Key]
-        public int ProjectId { get; set; }
+        public int projectId { get; set; }
+        public string projectName { get; set; }
+        public DateOnly creationDate { get; set; }
+        public string projectDescription { get; set; }
     }
 }
